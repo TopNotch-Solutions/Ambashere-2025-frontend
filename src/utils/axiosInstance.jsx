0b4e7d3493c91c2dabae984e520fe95e7f1e0c98
Paulus <pwilhem@mtc.com.na>
2026-06-05 13:35:37 +0200

    production url

--- src/utils/axiosInstance.jsx
+++ src/utils/axiosInstance.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:4000',
   //baseURL: 'http://172.19.13.140:4000' uat,
- baseURL: 'https://amberspharebackend.mtc.com.na', // prod
+ baseURL: 'https://amberspherebackend.mtc.com.na', // prod
   headers: { 'Content-Type': 'application/json' },
 });
 
--- src/utils/socket.jsx
+++ src/utils/socket.jsx
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-const socket = io.connect(process.env.REACT_APP_SOCKET_URL || "https://amberspharebackend.mtc.com.na", {
+const socket = io.connect(process.env.REACT_APP_SOCKET_URL || "https://amberspherebackend.mtc.com.na", {
     transports: ["websocket", "polling"],
     upgrade: false,
     debug: true,
